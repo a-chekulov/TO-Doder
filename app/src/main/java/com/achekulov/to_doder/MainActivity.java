23ad34e024f9563743eb1d8773ab2be9cb7e83a3
TOL00773 <tol00773@gmail.com>
2018-12-26 03:42:11 +0300

    fixed onCreateOptionsMenu

--- app/src/main/java/com/achekulov/to_doder/MainActivity.java
+++ app/src/main/java/com/achekulov/to_doder/MainActivity.java
@@ -25,18 +25,12 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        MenuItem splashItem = menu.findItem(R.id.action_splash);
+        splashItem.setChecked(preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE));
         return true;
     }
-
-//    @Override
-//    public boolean onCreateOptionsMenu(Menu menu) {
-//        // Inflate the menu; this adds items to the action bar if it is present.
-//        getMenuInflater().inflate(R.menu.menu_main, menu);
-//        MenuItem splashItem = menu.findItem(R.id.action_splash);
-//        splashItem.setChecked(preferenceHelper.getBoolean(PreferenceHelper.SPLASH_IS_INVISIBLE));
-//        return true;
-//    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
