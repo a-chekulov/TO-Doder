7f39a27589f9f9c1bbdfd7ad79a39320f87e6775
TOL00773 <tol00773@gmail.com>
2018-12-26 03:37:20 +0300

    add on/off a boot screen
    Created a boot screen and added an item in the action bar to show the splash screen or not

---
+++ app/src/main/java/com/achekulov/to_doder/PreferenceHelper.java
@@ -0,0 +1,45 @@
+package com.achekulov.to_doder;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+public class PreferenceHelper {
+    public final static String SPLASH_IS_INVISIBLE = "splash_is_invisible";
+    private static PreferenceHelper instance;
+    private Context context;
+    private SharedPreferences preferences;
+
+    private  PreferenceHelper(){
+    }
+
+    public static PreferenceHelper getInstance(){
+        if (instance == null) {
+            instance = new PreferenceHelper();
+        }
+        return instance;
+    }
+
+    public void init(Context context){
+        this.context = context;
+        preferences = context.getSharedPreferences("preferences", Context.MODE_PRIVATE);
+
+    }
+
+    public void putBoolean(String key, boolean value){
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    public boolean getBoolean(String key){
+        return preferences.getBoolean(key, false);
+    }
+
+    public Context getContext() {
+        return context;
+    }
+
+    public void setContext(Context context) {
+        this.context = context;
+    }
+}
---
+++ app/src/main/java/com/achekulov/to_doder/SplashFragment.java
@@ -0,0 +1,48 @@
+package com.achekulov.to_doder;
+
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.sql.Time;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+
+public class SplashFragment extends Fragment {
+
+    public SplashFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        SplashTask splashTask = new SplashTask();
+        splashTask.execute();
+
+        return inflater.inflate(R.layout.fragment_splash, container, false);
+    }
+
+    class SplashTask extends AsyncTask<Void, Void, Void>{
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            try {
+                TimeUnit.SECONDS.sleep(2);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            Objects.requireNonNull(getActivity()).getSupportFragmentManager().popBackStack();
+            return null;
+        }
+    }
+}
